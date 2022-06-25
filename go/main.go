e8327bfa5f9cee2340e392373ecfdec00ffe04d8
tomas-mota <tomasrebelomota@gmail.com>
2022-06-25 12:39:50 +0200

    remove file write

--- go/main.go
+++ go/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
-	err := os.WriteFile("test", []byte("hello world (go)"), 0644)
 	fmt.Println("hello dagger")
-	if err != nil {
-		log.Fatal(err)
-	}
 }
